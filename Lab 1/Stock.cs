5de2248176351d7b5c5d7a2955b7433438bed433
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-14 22:40:59 -0700

    Revert "include hint from prof error in stockBroker"
    This reverts commit 887d87bd9e943ba007e6307a802677f6a1d23f24.

--- Lab 1/Stock.cs
+++ Lab 1/Stock.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 namespace Lab_1
 {
     public class Stock
@@ -47,25 +46,19 @@ namespace Lab_1
             }
         }
         
-        // Change the stock value and invoke event to notify stock brokers when the threshold is reach
-        private void ChangeStockValue()
+        /// <summary>
+        /// Changes the stock value and also raising the event of stock value changes
+        /// </summary>
+        public void ChangeStockValue()
         {
-            // Generate a random number to within a range that stock can change every time unit and add it to the current stock's value
-            Random rand = new Random();
-            CurrentValue += rand.Next(-MaxChange, MaxChange);
-
-            // Increase the number of changes in value by 1
+            var rand = new Random().Next(-MaxChange, MaxChange);
+            CurrentValue += rand;
             NumChanges++;
-
-            // Check if the threshold is reached
-            if (Math.Abs(CurrentValue - InitialValue) >= Threshold)
+            if ((CurrentValue - InitialValue) > Threshold)
             {
-                // Raise the events
-                Parallel.Invoke(() => StockEvent?.Invoke(StockName, InitialValue, CurrentValue, NumChanges, DateTime.Now),
-                                () => StockEventData?.Invoke(this, new EventData(StockName, InitialValue, CurrentValue, NumChanges, DateTime.Now)));
+                StockNotification note = StockNotification;
+                StockEvent?.Invoke(this, note);
             }
         }
-
     }
-
 }
--- Lab 1/StockBroker.cs
+++ Lab 1/StockBroker.cs
@@ -11,10 +11,6 @@ namespace Lab_1
         public string BrokerName { get; set; }
 
         public List<Stock> stocks = new List<Stock>();
-        private static Mutex _locker = new Mutex();
-
-        // Path of the textfile to save the stock's information when the threshold is reached
-        private readonly string _docPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())));
 
         public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
         readonly string docPath = @"C:\Users\nguye\Documents\GitHub\CECS475_codeLab\Lab 1\Lab3_output.txt";
@@ -30,40 +26,15 @@ namespace Lab_1
             BrokerName = brokerName;
         }
 
-
+        /// <summary>
+        ///     Adds stock objects to the stock list
+        /// </summary>
+        /// <param name="stock">Stock object</param>
         public void AddStock(Stock stock)
         {
-            // Add this stock to the list of stocks controlled by the stock broker
             stocks.Add(stock);
-
-            // Subscribe to the events
-            stocks.StockEvent += Notify;
-            stocks.StockEventData += WriteToFile;
-
+            stock.StockEvent += EventHandler;
         }
-        private void Notify(string stockName, int initialValue, int currentValue, int numOfChanges, DateTime currentTime)
-        {
-            Console.WriteLine(BrokerName.PadRight(20) + stockName.PadRight(20) + initialValue.ToString().PadRight(20) + currentValue.ToString().PadRight(20) + numOfChanges.ToString().PadRight(20) + currentTime);
-        }
-
-        // Output to a textfile the broker's name and the stock's name, initial value, current value, number of changes in value, and time when the threshold is reached
-        private void WriteToFile(object sender, EventData e)
-        {
-            try
-            {
-                // Wait for the resource to be free
-                lock (_locker)
-                {
-                    using (FileStream file = new FileStream(Path.Combine(_docPath, "output.txt"), FileMode.Append, FileAccess.Write, FileShare.Read))
-                    using (StreamWriter outputFile = new StreamWriter(file))
-                    {
-                        outputFile.WriteLine(BrokerName.PadRight(20) + e.StockName.PadRight(20) + e.InitialValue.ToString().PadRight(20) + e.CurrentValue.ToString().PadRight(20) + e.NumOfChanges.ToString().PadRight(20) + e.CurrentTime);
-                    }
-                }
-            }
-            catch (IOException) { }
-        }
-
 
         /// <summary>
         ///     The eventhandler that raises the event of a change
@@ -82,6 +53,6 @@ namespace Lab_1
             {//file processing HERE
 
             }
-        }
     }
+    } 
 }
