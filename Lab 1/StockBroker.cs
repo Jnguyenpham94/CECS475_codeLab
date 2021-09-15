6fbd68375f65dd4196a21a33d91ce722bb9daa39
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-14 23:26:04 -0700

    DONE... I guess

--- Lab 1/StockBroker.cs
+++ Lab 1/StockBroker.cs
@@ -15,8 +15,6 @@ namespace Lab_1
         private readonly string _docPath =
         Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())));
 
-        public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) + "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
-
         public Mutex _locker = new Mutex();
 
         /// <summary>
@@ -34,16 +32,19 @@ namespace Lab_1
             // Add this stock to the list of stocks controlled by the stock broker
             stocks.Add(stock);
             // Subscribe to the events
-            //stock.StockEvent += Notify;
+            stock.StockEvent += Notify;
             stock.StockEventData += WriteToFile;
         }
-        private void Notify(string stockName, int initialValue, int currentValue, int
-        numOfChanges, DateTime currentTime)
+
+
+        private void Notify(object sender, EventData e)
         {
-            Console.WriteLine(BrokerName.PadRight(20) + stockName.PadRight(20) +
-            initialValue.ToString().PadRight(20) + currentValue.ToString().PadRight(20) +
-            numOfChanges.ToString().PadRight(20) + currentTime);
+            Console.WriteLine(BrokerName.PadRight(20) + e.StockName.PadRight(20) +
+            e.InitialValue.ToString().PadRight(20) + e.CurrentValue.ToString().PadRight(20) +
+            e.NumOfChanges.ToString().PadRight(20) + e.CurrentTime);
         }
+
+
         // Output to a textfile the broker's name and the stock's name, initial value, current value, number of changes in value, and time when the threshold is reached
         private void WriteToFile(object sender, EventData e)
         {
