e8c02cb058bc32eec80392532de44499f5ae8efd
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-18 23:51:50 -0700

    rebuild

--- Names/obj/Release/net5.0-windows/MainWindow.g.cs
+++ Names/obj/Release/net5.0-windows/MainWindow.g.cs
@@ -77,7 +77,7 @@ namespace Names {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Names;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Names;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
