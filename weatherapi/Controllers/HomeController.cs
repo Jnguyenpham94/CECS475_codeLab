9986710e4756799caa293a74d877814174324dda
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-11-29 23:24:13 -0800

    prof weatherapi starter code
    runs and displays basic weather stuff

---
+++ weatherapi/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using call_json_api.Models;
+using Polly;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
+
+namespace call_json_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+                
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {            
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+        
+
+        private async Task<object> GetWeatherForecasts()
+        {
+            // Get an instance of HttpClient from the factpry that we registered
+            // in Startup.cs
+            var client = _httpClientFactory.CreateClient("API Client");
+            // string s = "sacramento";
+            string s = "los angeles";
+            String apiUrl = "/api/location/search/?query=" + s;
+            int loc = 0;
+            //  List<Location> li=null;
+             var result1 = await client.GetAsync(apiUrl);
+            if (result1.IsSuccessStatusCode)
+            {
+                // Read all of the response and deserialise it into an instace of
+                // WeatherForecast class
+                var content1 = await result1.Content.ReadAsStringAsync();
+                List<Location> model1 = JsonConvert.DeserializeObject<List<Location>>(content1);
+              //  Location m1 = model1.consolidated_location[0];
+                //Location m2 = m1.consolidated_location[3];
+                loc = model1[0].woeid;
+              //  loc=model1[0].consolidated_location[3].woeid;
+                Debug.WriteLine(loc);
+              //  foreach(LocationArray i in model1)
+              //  {
+              //      loc = i.consolidated_location[3].woeid;                    }
+                   // i.consolidated_location.woeid;
+              //  }
+               // loc = model1.consolidated_location[0].woeid;
+
+                
+           }
+            Debug.WriteLine(loc);
+            string url = "/api/location/" + loc + "/";
+            var result = await client.GetAsync(url);
+           if (result.IsSuccessStatusCode)
+            {
+                // Read all of the response and deserialise it into an instace of
+                // WeatherForecast class
+                var content = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<WeatherForecast>(content);
+            }
+            return null;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await GetWeatherForecasts();
+            // Pass the data into the View
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ weatherapi/README.md
@@ -0,0 +1,25 @@
+
+This is a sample project to go with the article "[How to call a JSON API and display the result in ASP.NET Core MVC][1]"
+
+
+To get it working locally, download / `git clone` the code, then from the code folder run the
+followning:
+
+```
+dotnet restore
+dotnet run
+```
+
+And open [https://localhost:5001/][2] in your browser.
+
+### License ###
+Copyright (C) 2019 Artem Skvira
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[1]:https://nimblegecko.com/how-to-call-json-api-and-display-result-in-asp-net-core-mvc/
+[2]:https://localhost:5001/
